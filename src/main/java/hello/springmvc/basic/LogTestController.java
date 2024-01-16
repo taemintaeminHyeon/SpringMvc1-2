0b95d1ca6ac5d31ba41efda392584e8511b9d34b
taemintaeminHyeon <taemin14215@gmail.com>
2024-01-16 17:44:52 +0900

    test Commit

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,19 +1,27 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogTestController {
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    //private final Logger log = LoggerFactory.getLogger(getClass());
 
 
     @RequestMapping("/log-test")
     public String logTest() {
         String name = "Spring";
+
+        // log.trace("trace my log = " + name); 호출이 일어나지 않아도  + 연산이 일어나 메모리를 사용한다.
+        log.trace("trace log = {}",name);
+        log.debug("debug log = {}",name);
         log.info("info log = {}",name);
+        log.warn("warn log = {}",name);
+        log.error("error log = {}",name);
 
         return "ok";
 
---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,89 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "hello-basic", method = RequestMethod.GET)
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "OK!";
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable String userId) {
+        log.info("mappingPath userId = {} ",userId);
+        return "OK!";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long
+            orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    /**
+     * 파라미터로 추가 매핑
+     * params="mode",
+     * params="!mode"
+     * params="mode=debug"
+     * params="mode!=debug" (! = )
+     * params = {"mode=debug","data=good"}
+     * */
+    @GetMapping(value = "/mapping-param", params = "mode=debug") // 특정 파라미터가 존재하면 매핑
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    /**
+     * 특정 헤더로 추가 매핑
+     * headers="mode",
+     * headers="!mode"
+     * headers="mode=debug"
+     * headers="mode!=debug" (! = ) */
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    /**
+     * Content-Type 헤더 기반 추가 매핑 Media Type * consumes="application/json"
+     * consumes="!application/json"
+     * consumes="application/*"
+     * consumes="*\/*"
+     * MediaType.APPLICATION_JSON_VALUE
+     */
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    /**
+     * Accept 헤더 기반 Media Type * produces = "text/html"
+     * produces = "!text/html"
+     * produces = "text/*"
+     * produces = "*\/*"
+     */
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
